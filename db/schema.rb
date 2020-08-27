bf90271001666b346fac7b99e7a47ade2bbb83a7
araki174 <arakiwork484@gmail.com>
2020-08-27 19:05:31 +0900

    deviseに関するビューファイルの配置

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 2020_08_27_090142) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "password_Confirmation", null: false
+    t.string "first_name", null: false
+    t.string "rast_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "rast_name_kana", null: false
+    t.string "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

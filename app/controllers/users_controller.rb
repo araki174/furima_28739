f6163fc6c1d2f12f68fa3cd0325fcd61ba2440b3
araki174 <arakiwork484@gmail.com>
2020-08-31 18:08:27 +0900

    ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,10 @@ class UsersController < ApplicationController
   def index
   end
 
-  def edit
-  end
-
   def show
     user = User.find(params[:id])
     @nickname = current_user.nickname
     @email = current_user.email
   end
 
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).parmit(:user, :email)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  root to: "users#index"
+  get 'items/index'
+  # get 'users/index'
+  root to: "items#index"
   resources :users, only: [:edit, :update]
   devise_for :users
 end

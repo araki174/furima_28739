d0ce328b9d8d215d93a88074654d901d2352108a
araki174 <arakiwork484@gmail.com>
2020-08-31 19:07:18 +0900

    ログアウト機能の実装（修正）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  # get 'users/index'
+  # get 'items/index'
   root to: "items#index"
   resources :users, only: [:edit, :update]
   devise_for :users

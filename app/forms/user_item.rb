25fbcc0690561aaeea8bbc46f037f172e137331f
araki174 <arakiwork484@gmail.com>
2020-09-10 20:58:14 +0900

    heroku用　修正1

--- app/forms/user_item.rb
+++ app/forms/user_item.rb
@@ -1,8 +1,8 @@
-class UserDonation
+# class UserDonation
 
-  include ActiveModel::Model
-  attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
+#   include ActiveModel::Model
+#   attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
 
-  Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, 
-    building_name: building_name, user_id: user.id)
-end
+#   Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, 
+#     building_name: building_name, user_id: user.id)
+# end

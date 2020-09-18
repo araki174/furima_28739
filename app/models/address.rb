780459bd7eff8bc1edcd77f2e7daaa2b0b624087
araki174 <arakiwork484@gmail.com>
2020-09-18 10:22:23 +0900

    商品購入機能の単体テストコード

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :order
   
-  with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, format: { with: /[^0]/ }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :house_number
-    validates :phone_number
-  end
+  # with_options presence: true do
+  #   validates :postal_code,   format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  #   validates :prefecture_id, format: { with: /[^0]/ }
+  #   validates :city,          format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  #   validates :house_number   
+  #   validates :phone_number   format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  # end
 
 end

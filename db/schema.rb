2550a15d51d0f86f5505ac8362e0344f07428d72
araki174 <arakiwork484@gmail.com>
2020-09-18 12:38:18 +0900

    電話番号の型修正

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_023342) do
     t.string "city", default: "", null: false
     t.string "house_number", default: "", null: false
     t.string "building_name", default: ""
-    t.integer "phone_number"
+    t.string "phone_number", default: ""
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
